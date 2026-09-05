13f3f3f312b059360432bdbe25825de8fb30b265
Satyamkrsingh27 <satyamkrsingh27@gmail.com>
2026-09-05 16:03:53 +0530

    Program 1

---
+++ week5/program1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class program1 {
+  public static void curveScores(int scores[], int num) {
+    for (int i = 0; i < scores.length; i++) {
+      scores[i] = scores[i] + num;
+    }
+    System.out.println(Arrays.toString(scores));
+  }
+
+  public static void main(String[] arg) {
+    int[] scores = { 70, 85, 60 };
+    curveScores(scores, 10);
+  }
+
+}
